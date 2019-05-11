4ce317a07d9b85d401e11f9ffdc3953b4780cdd9
master <1790076682@qq.com>
2019-05-11 10:44:58 +0800

    66666

--- app/src/main/java/com/example/dome02/MainActivity.java
+++ app/src/main/java/com/example/dome02/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //打算的撒地方
-        //大师法撒旦法
+        //我是分支
+
     }
 }
